a10573de9034b65f2868ef0ee4873b6b635e2fa9
memothelemo <memothelemo@protonmail.com>
2023-06-30 22:21:17 +0800

    test(settings): check for variables that are not aliased

--- crates/shared/src/settings/impls.rs
+++ crates/shared/src/settings/impls.rs
@@ -92,18 +92,28 @@ mod tests {
 
   use figment::Jail;
 
+  fn generate_bot_token() -> String {
+    // this is to fix the error
+    let mut random_value = rand::random::<u64>().to_string();
+    random_value.push('c');
+    random_value
+  }
+
   #[test]
   fn prefix_env_aliases() {
     Jail::expect_with(|jail| {
       const POOL_TIMEOUT_SECS: u64 = 10;
       const POOL_SIZE: u64 = 30;
 
+      let generated_token = generate_bot_token();
       jail.set_env("POKEPET_DATABASE_POOL_TIMEOUT_SECS", POOL_TIMEOUT_SECS);
       jail.set_env("POKEPET_DATABASE_POOL_SIZE", POOL_SIZE);
+      jail.set_env("POKEPET_BOT_TOKEN", generated_token);
 
       let settings = Settings::figment().extract::<Settings>().unwrap();
       assert_eq!(settings.database.pool_timeout_secs.get(), POOL_TIMEOUT_SECS);
       assert_eq!(settings.database.pool_size.get(), POOL_SIZE);
+      assert_eq!(settings.bot.token, generated_token);
 
       Ok(())
     });
@@ -114,10 +124,7 @@ mod tests {
     // This to avoid false positives
     Jail::expect_with(|jail| {
       for bot_token_alias in BOT_TOKEN_ALIASES {
-        let mut random_value = rand::random::<u64>().to_string();
-
-        // this is to fix the error
-        random_value.push('c');
+        let random_value = generate_bot_token();
         jail.set_env(bot_token_alias, &random_value);
 
         let settings = Settings::figment().extract::<Settings>().unwrap();
